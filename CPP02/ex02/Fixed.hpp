81c85a8d952e2f2e06552f2c45c8abc26226b9cd
ahokcool <stein.alexander@posteo.de>
2024-03-13 21:19:41 +0000

    auto commit: ex02: done

---
+++ CPP02/ex02/Fixed.hpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/09 21:22:36 by astein            #+#    #+#             */
+/*   Updated: 2024/03/13 17:24:03 by astein           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+
+	public:
+		// Constructors and destructor
+		Fixed();
+		Fixed(const Fixed &other);
+		Fixed(const int value);
+		Fixed(const float value);
+		~Fixed();
+		
+		// Copy assignment operator
+		Fixed 		&operator=(const Fixed &other);
+		
+		// Basic arithmetic operator overloads
+		Fixed		operator+(const Fixed &other) const;
+		Fixed		operator-(const Fixed &other) const;
+		Fixed		operator*(const Fixed &other) const;
+		Fixed		operator/(const Fixed &other) const;
+
+		// Increment and decrement operators overloads
+		Fixed		operator++();		// prefix
+		Fixed		operator--();		// prefix
+		Fixed		operator++(int);	// int is a dummy parameter to differentiate from the prefix version
+		Fixed		operator--(int);	// int is a dummy parameter to differentiate from the prefix version
+		
+		// Comparison operators overloads
+		bool		operator>(const Fixed &other) const;
+		bool		operator<(const Fixed &other) const;
+		bool		operator>=(const Fixed &other) const;
+		bool		operator<=(const Fixed &other) const;
+		
+		// Additional comparison operators overloads
+		static Fixed		&min(Fixed &a, Fixed &b);
+		static const Fixed	&min(const Fixed &a, const Fixed &b);
+		static Fixed		&max(Fixed &a, Fixed &b);
+		static const Fixed	&max(const Fixed &a, const Fixed &b);
+		
+		// Getters and Setters
+		int						getRawBits() const;
+		void					setRawBits(const int raw);
+		float					toFloat() const;
+		int						toInt() const;
+		
+	private:
+		int					_value;
+		static const int	_fractionalBits;
+};
+
+	std::ostream 	&operator<<(std::ostream &os, const Fixed &fixed);
+#endif
---
+++ CPP02/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/13 16:46:21 by astein            #+#    #+#             */
+/*   Updated: 2024/03/13 16:47:30 by astein           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int	main( void )
+{
+	Fixed		a;
+	Fixed const	b(Fixed(5.05f) * Fixed(2));
+	
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return 0;
+}

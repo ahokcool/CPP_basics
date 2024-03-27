6ebbcb4b935616611a1cd3a6c9537ebcb6d504f0
ahokcool <stein.alexander@posteo.de>
2024-03-27 19:31:02 +0000

    auto commit: ex03: started...

---
+++ CPP04/ex03/AMateria.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/27 19:17:13 by astein            #+#    #+#             */
+/*   Updated: 2024/03/27 19:30:37 by astein           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AAANIMAL_HPP
+# define AAANIMAL_HPP
+
+# include <iostream>
+# include "Brain.hpp"
+
+class AAnimal
+{
+	// TODO
+};
+
+#endif

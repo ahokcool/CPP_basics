cc1fd9b82ff76b1f4fd82bb204f324d0d37df990
ahokcool <stein.alexander@posteo.de>
2024-03-29 22:10:00 +0000

    auto commit: harl: updated syntax

--- CPP01/ex05/Harl.hpp
+++ CPP01/ex05/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 16:33:11 by astein            #+#    #+#             */
-/*   Updated: 2024/03/09 17:13:09 by astein           ###   ########.fr       */
+/*   Updated: 2024/03/29 22:07:17 by astein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,18 @@
 
 class Harl
 {
+	public:
+		Harl();
+		~Harl();
+
+		// Member function
+		void	complain(const std::string level) const;
+		
 	private:
 		void _debug() const;
 		void _info() const;
 		void _warning() const;
 		void _error() const;
-		
-	public:
-		Harl();
-		~Harl();
-
-		void	complain(const std::string level) const;
 };
 
 #endif

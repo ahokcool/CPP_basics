3950dca21177e66b1521e0ff4a71c55a14c12a4f
ahokcool <stein.alexander@posteo.de>
2024-03-29 23:29:38 +0000

    auto commit: ex02: added more tests

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 16:46:21 by astein            #+#    #+#             */
-/*   Updated: 2024/03/13 16:47:30 by astein           ###   ########.fr       */
+/*   Updated: 2024/03/29 23:29:03 by astein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,43 @@ int	main( void )
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
+	// More tests
+	a = 42;
+	std::cout << a << std::endl;
+	a = a - 1;
+	std::cout << a << std::endl;
+	a = a + 1;
+	std::cout << a << std::endl;
+	a = a * 2;
+	std::cout << a << std::endl;
+	a = a / 2;
+	std::cout << a << std::endl;
+	
+	// Comparison operators
+	Fixed c(41);
+	Fixed d(42);
+	std::cout << "c = " << c << std::endl;
+	std::cout << "d = " << d << std::endl;
+	std::cout << "c > d: " << (c > d) << std::endl;
+	std::cout << "c < d: " << (c < d) << std::endl;
+	std::cout << "c >= d: " << (c >= d) << std::endl;
+	std::cout << "c <= d: " << (c <= d) << std::endl;
+	
+	// Min and Max
+	std::cout << "min(c, d) = " << Fixed::min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << Fixed::max(c, d) << std::endl;
+	const Fixed e(41);
+	const Fixed f(42);
+	std::cout << "e = " << e << std::endl;
+	std::cout << "f = " << f << std::endl;
+	std::cout << "min(e, f) = " << Fixed::min(e, f) << std::endl;
+	std::cout << "max(e, f) = " << Fixed::max(e, f) << std::endl;
+	
+	// ToInt and ToFloat
+	Fixed g(12.3456789f);
+	std::cout << "g = " << g << std::endl;
+	std::cout << "g.toInt() = " << g.toInt() << std::endl;
+	std::cout << "g.toFloat() = " << g.toFloat() << std::endl;
+	
 	return 0;
 }

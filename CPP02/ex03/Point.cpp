d7927a290cd4355fbc6ffcfad2a6f9657dc1d849
ahokcool <stein.alexander@posteo.de>
2024-03-29 23:38:11 +0000

    auto commit: ex03: updated syntax

--- CPP02/ex03/Point.cpp
+++ CPP02/ex03/Point.cpp
@@ -6,32 +6,37 @@
 /*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 21:20:29 by astein            #+#    #+#             */
-/*   Updated: 2024/03/13 23:22:38 by astein           ###   ########.fr       */
+/*   Updated: 2024/03/29 23:37:57 by astein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
+// Default constructor
 Point::Point() : _x(0), _y(0)
 {
 	// Nothing to do here
 }
 
+// Parameterized constructor
 Point::Point(const float x, const float y) : _x(x), _y(y)
 {
 	// Nothing to do here
 }
 
+// Copy constructor
 Point::Point(const Point &other) : _x(other._x), _y(other._y)
 {
 	// Nothing to do here
 }
 
+// Destructor
 Point::~Point()
 {
 	// Nothing to do here
 }
 
+// Member functions
 float	Point::get_x() const
 {
 	return _x.toFloat();

cebd0245339a1a584bd13b3c9ad29e05576b89bc
ahokcool <stein.alexander@posteo.de>
2024-04-21 14:12:09 +0100

    auto commit: ex00: improved main tests

---
+++ CPP07/ex00/Data.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Data.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/18 13:39:28 by astein            #+#    #+#             */
+/*   Updated: 2024/04/21 14:09:05 by astein           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Data.hpp"
+
+// Constructors & Destructors
+Data::Data() : _name("Default"), _moreData(42)
+{
+	// Nothing to do here
+}
+
+Data::Data(const std::string &name, int moreData) : _name(name), _moreData(moreData)
+{
+	// Nothing to do here
+}
+
+Data::Data(const Data &other) : _name(other._name), _moreData(other._moreData)
+{
+	// Nothing to do here
+}
+
+Data::~Data()
+{
+	// Nothing to do here
+}
+
+// Equal assignment operator overload
+Data 	&Data::operator=(const Data &other)
+{
+	if (this == &other)
+		return *this;
+	_name = other._name;
+	_moreData = other._moreData;
+	return *this;
+}
+
+bool 	Data::operator<(const Data &other) const
+{
+	if(this->_moreData < other._moreData)
+		return true;
+	return false;
+}
+
+bool 	Data::operator>(const Data &other) const
+{
+	if(this->_moreData > other._moreData)
+		return true;
+	return false;
+}
+
+// Member functions
+void	Data::printData() const
+{
+	std::cout <<
+		"Hello, my name is: " <<
+		 _name <<
+		 "\t and I have more data: " <<
+		 _moreData << std::endl;
+}
+
+void	Data::setName(const std::string &name)
+{
+	_name = name;
+}
--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,14 +6,46 @@
 /*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 18:27:16 by astein            #+#    #+#             */
-/*   Updated: 2024/04/20 18:51:30 by astein           ###   ########.fr       */
+/*   Updated: 2024/04/21 14:11:50 by astein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+#include "Data.hpp"
+
+// COLORS
+# define CLR_RED "\033[31m"
+# define CLR_GRN "\033[32m"
+# define CLR_YLW "\033[33m"
+# define CLR_BLU "\033[34m"
+# define CLR_ORN "\033[38;5;202m"
+# define CLR_BLD "\033[1m"
+# define CLR_RST "\033[0m"
+
+void	title(std::string str, bool newline_before, bool newline_after)
+{
+	if (newline_before)
+		std::cout << std::endl;
+	std::cout << CLR_ORN <<
+		">>> " << str << " <<<" <<
+		CLR_RST << std::endl;
+	if (newline_after)
+		std::cout << std::endl;
+}
+
+void	info(std::string str, std::string clr)
+{
+	std::cout <<
+		clr <<
+		" >> " << str <<
+		CLR_RST << std::endl;
+}
+
 
 int	main( void )
 {
+	
+	title("START: 42 EXAMPLE", true, false);
 	int a = 2;
 	int a2 = 2;
 	int b = 3;
@@ -31,18 +63,19 @@ int	main( void )
 
 	std::cout << "min( a, a2 ) = " << ::min( a, a2 ) << std::endl;
 	std::cout << "max( a, a2 ) = " << ::max( a, a2 ) << std::endl;
+	title("END: 42 EXAMPLE", false, true);
 
-	std::cout << "TEST SWAP" << std::endl;
+	title("START: MY EXAMPLE", true, false);
+	info("TEST SWAP", CLR_GRN);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	int x = 3000000;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "x = " << x << std::endl;
-	std::cout << "TEST SWAP" << std::endl;
 	::swap(a, x);
 	std::cout << "x = " << x << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 
-	std::cout << "TEST WITH STRINGS" << std::endl;
+	info("TEST WITH STRINGS", CLR_GRN);
 	std::string s1 ("Hello!");
 	std::string s2 ("Helli!");
 	std::string s3 ("Hell!");
@@ -51,7 +84,7 @@ int	main( void )
 	std::cout << "min( " << s1 << ", " << s3 << " ) = " << ::min( s1, s3 ) << std::endl;
 	std::cout << "max( " << s1 << ", " << s3 << " ) = " << ::max( s1, s3 ) << std::endl;
 
-	std::cout << "TEST WITH POINTER" << std::endl;
+	info("TEST WITH POINTER", CLR_GRN);
 	std::string *p1 = &s1;
 	std::string *p2 = &s2;
 	std::string *p3 = &s3;
@@ -64,5 +97,28 @@ int	main( void )
 	std::cout << "max( " << *p1 << ", " << *p2 << " ) = " << *::max( p1, p2 ) << std::endl;
 	std::cout << "min( " << *p1 << ", " << *p3 << " ) = " << *::min( p1, p3 ) << std::endl;
 	std::cout << "max( " << *p1 << ", " << *p3 << " ) = " << *::max( p1, p3 ) << std::endl;
+	title("END: MY EXAMPLE", false, true);
+	{
+		title("START: TEST WITH INSTANCES OF CLASS", true, false);
+		Data d1("Alex", 42);
+		Data d2("John1", 24);
+		Data d3("John2", 24);
+		d1.printData();
+		d2.printData();
+		info("TEST SWAP", CLR_GRN);
+		::swap(d1, d2);
+		d1.printData();
+		d2.printData();
+		info("TEST MIN", CLR_GRN);
+		::min(d1, d2).printData();
+		info("TEST MAX", CLR_GRN);
+		::max(d1, d2).printData();
+		info("TEST MIN/MAX with same values (should return the second argument!)", CLR_GRN);
+		::swap(d1, d2);
+		::min(d2, d3).printData();
+		::max(d2, d3).printData();
+		::min(d3, d2).printData();
+		::max(d3, d2).printData();
+	}
 	return 0;
 }

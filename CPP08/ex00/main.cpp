345ac3040160a8e087103d8544f7b5811aa0c6e3
ahokcool <stein.alexander@posteo.de>
2024-04-23 17:03:15 +0100

    auto commit: ex00: done

--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -6,15 +6,27 @@
 /*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 15:16:57 by astein            #+#    #+#             */
-/*   Updated: 2024/04/23 16:52:04 by astein           ###   ########.fr       */
+/*   Updated: 2024/04/23 17:03:04 by astein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 #include <vector>
+#include <list>
+
+// COLORS
+# define CLR_RED "\033[31m"
+# define CLR_GRN "\033[32m"
+# define CLR_YLW "\033[33m"
+# define CLR_BLU "\033[34m"
+# define CLR_ORN "\033[38;5;202m"
+# define CLR_BLD "\033[1m"
+# define CLR_RST "\033[0m"
 
 int main()
 {
+	std::cout << "TEST WITH VECTOR CONTAINER" << std::endl;
+
 	std::vector<int> vi;
 	vi.push_back(42);
 	vi.push_back(1);
@@ -34,8 +46,40 @@ int main()
 		std::cerr << e.what() << '\n';
 	}
 	
+	std::cout << "TEST WITH LIST CONTAINER" << std::endl;
 
-	
+	std::list<int> list;
+
+    list.push_back(1);
+    list.push_back(5);
+    list.push_back(12);
+    list.push_back(23);
+    list.push_back(23);
+    list.push_back(30);
+
+    std::list<int>::iterator it;
+
+    for (it = list.begin(); it != list.end(); ++it)
+        std::cout << &(*it) << std::endl;
+    
+
+    for (int i = 0; i <= 30; i++)
+    {
+		try
+		{
+            std::cout <<
+				"does value '" << i << "' exists on the container? " << CLR_GRN <<
+				::easyfind(list, i) <<
+				std::endl << CLR_RST;
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << CLR_RED <<
+				"Exception cought: couldn't find: " <<
+				i << 
+				std::endl << CLR_RST;
+		}            
+    }
 	return 0;
 }
 

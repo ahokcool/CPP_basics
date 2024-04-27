5f553033ac728ff42ca4a89c17b229dcfd6f9aba
ahokcool <stein.alexander@posteo.de>
2024-04-27 18:51:18 +0100

    auto commit: btc: started...

---
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/27 18:01:56 by astein            #+#    #+#             */
+/*   Updated: 2024/04/27 18:02:23 by astein           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# define CSV_FILE "data.csv"
+
+#endif
